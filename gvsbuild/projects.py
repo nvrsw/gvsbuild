b440835306943fea3873d03e85e693b417cbba57
Chungrae Cho <ccr@emstone.com>
2020-04-10 11:59:21 +0900

    cairo: update library version to 1.17.2

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -84,8 +84,8 @@ class Project_cairo(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'cairo',
-            archive_url = 'https://cairographics.org/releases/cairo-1.16.0.tar.xz',
-            hash = '5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331',
+            archive_url = 'https://cairographics.org/snapshots/cairo-1.17.2.tar.xz',
+            hash = '6b70d4655e2a47a22b101c666f4b29ba746eda4aa8a0f7255b32b2e9408801df',
             dependencies = ['fontconfig', 'glib', 'pixman', 'libpng'],
             patches = ['001-win32-font-corruption.patch'],
             )
