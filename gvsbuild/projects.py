0d6dbe15077c3ab5798b216af42c9d63674b667b
Chungrae Cho <ccr@emstone.com>
2024-10-31 11:02:17 +0900

    glib: remove dependency pcre2 library
    I checked the source code for the latest version of glib and found
    that automatically checks for external PCRE2 libraries and uses
    If it doesn't exist, it seems to use internal code

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -461,7 +461,7 @@ class Project_glib(Tarball, Meson):
             'glib',
             archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/glib/2.82/glib-2.82.2.tar.xz',
             hash = 'ab45f5a323048b1659ee0fbda5cecd94b099ab3e4b9abf26ae06aeb3e781fd63',
-            dependencies = ['ninja', 'meson', 'pkg-config', 'gettext', 'libffi', 'zlib', 'pcre2'],
+            dependencies = ['ninja', 'meson', 'pkg-config', 'gettext', 'libffi', 'zlib'],
             patches = [
                 '001-glib-package-installation-directory.patch',
                 '0001-gsocket-windows-check-event-before-calling-WSAEnumNe.patch',
@@ -476,21 +476,6 @@ class Project_glib(Tarball, Meson):
     def build(self):
         Meson.build(self)
         self.install(r".\LICENSES\* share\doc\glib")
-
-@project_add
-class Project_pcre2(Tarball, Meson):
-    def __init__(self):
-        Project.__init__(self,
-            'pcre2',
-            archive_url='https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.44/pcre2-10.44.tar.gz',
-            hash='86b9cb0aa3bcb7994faa88018292bc704cdbb708e785f7c74352ff6ea7d3175b',
-            dependencies=['ninja', 'meson', 'pkg-config'],
-            patches = [],
-        )
-
-    def build(self):
-        Meson.build(self)
-        self.install(r".\COPYING share\doc\pcre2")
 
 @project_add
 class Project_glib_networking(Tarball, Meson):
