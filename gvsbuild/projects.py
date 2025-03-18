0f10fb897f207bd73daa8fa07bd3a1c6791a5b70
Chungrae Cho <ccr@emstone.com>
2025-03-18 10:29:28 +0900

    gperf: use a mirrored repository
    "https://gitlab.freedesktop.org/tpm/gperf.git" 저장소를
    사용하지 않고 미러링된 "https://github.com/rurban/gperf.git"
    저장소를 사용하도록 합니다.

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -341,9 +341,9 @@ class Gperf(GitRepo, Meson):
         Meson.__init__(
             self,
             "gperf",
-            repo_url="https://github.com/hsccr/gperf.git",
+            repo_url="https://github.com/rurban/gperf.git",
             fetch_submodules=False,
-            tag = None,
+            tag="c24359b4eab86d71c655c3b3fc969f13aac879ce",
             dependencies=["ninja"],
         )
 
